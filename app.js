9442455060f27bf6edee246ef9523a7ab4b6d03e
Vo Pham Minh Tan <minhtan@Vos-MBP-2.lan>
2021-05-13 15:30:05 +0700

    setup app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-require(dotenv).config();
+const cors = require("cors");
+const utilsHelper = require("./helpers/utils.helper");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI = process.env.MONGODB_URI;
 
 var indexRouter = require("./routes/index");
 
@@ -12,8 +17,46 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api", indexRouter);
+
+/* DB Connections */
+mongoose
+  .connect(mongoURI, {
+    // some options to deal with deprecated warning
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Mongoose connected to ${mongoURI}`))
+  .catch((err) => console.log(err));
+
+// catch 404 and forard to error handler
+app.use((req, res, next) => {
+  const err = new Error("404 - Resource not found");
+  next(err);
+});
+
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+  console.log("ERROR", err);
+  const statusCode = err.message.split(" - ")[0];
+  const message = err.message.split(" - ")[1];
+  if (!isNaN(statusCode)) {
+    utilsHelper.sendResponse(res, statusCode, false, null, { message }, null);
+  } else {
+    utilsHelper.sendResponse(
+      res,
+      500,
+      false,
+      null,
+      { message: err.message },
+      "Internal Server Error"
+    );
+  }
+});
 
 module.exports = app;
